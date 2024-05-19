400df129d4d0ba6ad6804084654a9dedb1add2a5
felipejhordan <felipejordan.alves@gmail.com>
2024-05-19 17:16:45 -0300

    feat(backend-2): setup nest config in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayableModule } from './core/payable/payable.module';
+import { ConfigModule } from '@nestjs/config';
+import { configurationGlobal } from './common/configuration/config.global';
 
 @Module({
-  imports: [PayableModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+      load: [ configurationGlobal ]
+    }),
+    PayableModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
