759033de6c27de3babb3e328e3dda4d995e27696
felipejhordan <felipejordan.alves@gmail.com>
2024-05-21 13:16:34 -0300

    feat(backend-2): create constructor in domain entities

--- backend/src/core/assignor/domain/assignor.ts
+++ backend/src/core/assignor/domain/assignor.ts
@@ -1,5 +1,17 @@
 export class Assignor {
-  constructor() {}
+  constructor(
+    id: string,
+    document: string,
+    email: string,
+    phone: string,
+    name: string,
+  ) {
+    this.id = id;
+    this.document = document;
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
+  }
 
   id: string;
 
--- backend/src/core/payable/domain/payable.ts
+++ backend/src/core/payable/domain/payable.ts
@@ -1,4 +1,10 @@
 export class Payable {
+  constructor(id: string, value: number, emissionDate: Date, assignor: string) {
+    this.id = id;
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignor = assignor;
+  }
   id: string;
 
   value: number;
